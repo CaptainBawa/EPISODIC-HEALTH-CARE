635d43947354467a63593ddb9a6e6054c7d4af95
Collins Bawa <collinskwamebawa@yahoo.com>
2025-01-28 15:00:07 -0500

    Add hamburger items

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,43 @@
-import React from 'react';
+import React, { useState} from 'react';
 import ehc from '../assets/ehc.png';
 
-const Header = () => (
-  <header>
-    <div>
-      <img src={ehc} alt="Episodic Health Care" />
-    </div>
-    <div className="background">
-      <button className="menu__icon">
-        <span />
-        <span />
-        <span />
-      </button>
-    </div>
-  </header>
-);
+const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen((prev) => !prev);
+  };
+
+  return (
+    <header>
+      <div>
+        <img src={ehc} alt="Episodic Health Care" />
+      </div>
+      <div className="background">
+        <button className="menu__icon" onClick={toggleMenu}>
+        {menuOpen ? (
+            <div className="close-icon">X</div>
+          ) : (
+            <>
+              <span />
+              <span />
+              <span />
+            </>
+          )}
+        </button>
+      </div>
+      {menuOpen && (
+        <div className="menu__items">
+          <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Services</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+      )}
+    </header>
+  );
+};
 
 export default Header;
