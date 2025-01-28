95cef832df840912b2a84a5274af647d0ea0d1b1
Collins Bawa <collinskwamebawa@yahoo.com>
2025-01-28 14:14:47 -0500

    Add landing page basic code

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const LandingPage = () => (
+    <div>
+        <h1>Episodic Health Care</h1>
+        <p>Episodic Health Care is a new way to manage your health care.</p>
+    </div>
+    );
+
+    export default LandingPage;
