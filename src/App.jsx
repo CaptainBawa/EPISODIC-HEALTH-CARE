e5b33b9d33395e64298a93f3afd5b10db1890bca
Collins Bawa <collinskwamebawa@yahoo.com>
2025-01-30 13:40:22 -0500

    Import products component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
+import CarouselSlider from './components/CarouselSlider';
+import Products from './components/Products';
 import LandingPage from './components/LandingPage';
 
 const App = () => (
   <BrowserRouter>
     <Header />
+    <CarouselSlider />
+    <Products />
     <Routes>
       <Route path="/" element={<LandingPage />} />
     </Routes>
