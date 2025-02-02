b9dd7ceb36de95276c65d5d7a0ce0fc6032740ee
Collins Bawa <collinskwamebawa@yahoo.com>
2025-02-02 16:06:06 -0500

    Import testimonies component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Header from './components/Header';
 import CarouselSlider from './components/CarouselSlider';
 import Products from './components/Products';
 import LandingPage from './components/LandingPage';
+import Testimonies from './components/Testimonies';
 
 const App = () => (
   <BrowserRouter>
     <Header />
     <CarouselSlider />
     <Products />
+    <Testimonies />
     <Routes>
       <Route path="/" element={<LandingPage />} />
     </Routes>
