0ca08bc80c38d2bf814c9d6a8786ae909bb97c44
Collins Bawa <collinskwamebawa@yahoo.com>
2025-02-05 12:17:55 -0500

    Add basic footer code

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer>
+    <p>Developed and Designed by</p>
+    <p>Easing Life Group of Companies. 0240095360</p>
+  </footer>
+);
+
+export default Footer;
