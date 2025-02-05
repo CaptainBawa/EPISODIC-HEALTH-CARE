6e0eedfa83f72dd428d264015e1c9fd0b9c36b7e
Collins Bawa <collinskwamebawa@yahoo.com>
2025-02-05 12:16:26 -0500

    Import contact, services, and about us component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import Header from './components/Header';
 import CarouselSlider from './components/CarouselSlider';
 import Products from './components/Products';
-import LandingPage from './components/LandingPage';
+import Footer from './components/Footer';
 import Testimonies from './components/Testimonies';
 import Gallery from './components/Gallery';
+import locationRoutes from './components/LocationRoutes';
+import HealthcareDirections from './components/HealthcareDirections';
+import Contact from './components/Contact';
+import Services from './components/Services';
+import About from './components/AboutUs';
 
 const App = () => (
   <BrowserRouter>
@@ -13,9 +18,14 @@ const App = () => (
     <Products />
     <Testimonies />
     <Gallery />
-    <Routes>
+    <HealthcareDirections routes={locationRoutes} />
+    <Contact />
+    <Services />
+    <About />
+    <Footer />
+    {/* <Routes>
       <Route path="/" element={<LandingPage />} />
-    </Routes>
+    </Routes> */}
   </BrowserRouter>
 );
 
