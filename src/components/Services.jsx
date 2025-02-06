2154e360bbaa7878cee3bcf518fa189fd8d8f8df
Collins Bawa <collinskwamebawa@yahoo.com>
2025-02-06 12:45:21 -0500

    Add services component code

---
+++ src/components/Services.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Services = () => (
+  <div id="services">
+    <h2>Services</h2>
+    <ul>
+      <li>General Health Care</li>
+      {' | '}
+      <li>Scan Center</li>
+      {' | '}
+      <li>Laboratory</li>
+      {' | '}
+      <li>Physiotherapy</li>
+    </ul>
+  </div>
+);
+
+export default Services;
