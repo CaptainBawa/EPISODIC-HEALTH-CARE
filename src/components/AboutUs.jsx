b194028eb4e8b981d51efbcedc2d0da211e8b9f8
Collins Bawa <collinskwamebawa@yahoo.com>
2025-02-07 08:16:29 -0500

    Add about us section code

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const About = () => (
+  <div id="about">
+    <h2>About Us</h2>
+    <p>Easing Life Group of Companies. 0240095360</p>
+  </div>
+);
+
+export default About;
