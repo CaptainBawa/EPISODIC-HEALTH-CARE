ec425fdf08f412dce00d9f3256ef7fd4487e5798
Collins Bawa <collinskwamebawa@yahoo.com>
2025-02-09 13:20:35 -0500

    Import react toast

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Toast from './components/ToastContainer';
 import MainLayout from './components/MainLayout';
 import AdminPanel from './components/AdminPanel';
 
 const App = () => (
   <BrowserRouter>
+    <Toast />
     <Routes>
       <Route path="/" element={<MainLayout />} />
       <Route path="/admin" element={<AdminPanel />} />
