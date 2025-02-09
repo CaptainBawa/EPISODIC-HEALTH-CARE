9acdb3057476ad5e836c225320690583f954461d
Collins Bawa <collinskwamebawa@yahoo.com>
2025-02-09 12:40:05 -0500

    Restructure code

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,14 @@
-import { BrowserRouter } from 'react-router-dom';
-import Header from './components/Header';
-import CarouselSlider from './components/CarouselSlider';
-import Products from './components/Products';
-import Footer from './components/Footer';
-import Testimonies from './components/Testimonies';
-import Gallery from './components/Gallery';
-import locationRoutes from './components/LocationRoutes';
-import HealthcareDirections from './components/HealthcareDirections';
-import Contact from './components/Contact';
-import Services from './components/Services';
-import About from './components/AboutUs';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainLayout from './components/MainLayout';
+import AdminPanel from './components/AdminPanel';
 
 const App = () => (
   <BrowserRouter>
-    <Header />
-    <CarouselSlider />
-    <Products />
-    <Testimonies />
-    <Gallery />
-    <HealthcareDirections routes={locationRoutes} />
-    <Contact />
-    <Services />
-    <About />
-    <Footer />
-    {/* <Routes>
-      <Route path="/" element={<LandingPage />} />
-    </Routes> */}
+    <Routes>
+      <Route path="/" element={<MainLayout />} />
+      <Route path="/admin" element={<AdminPanel />} />
+    </Routes>
   </BrowserRouter>
 );
 
