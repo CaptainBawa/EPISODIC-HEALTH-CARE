d3e5ab7832523e6f32f9a8c805eb98b228603bf8
Collins Bawa <collinskwamebawa@yahoo.com>
2025-02-11 13:56:30 -0500

    Import new images

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,5 +1,13 @@
 import React, { useState } from 'react';
-import medi from '../assets/medi.jpg';
+import opd from '../assets/opd.jpg';
+import despen from '../assets/despen.jpg';
+import lab from '../assets/lab.jpg';
+import scan from '../assets/scan.jpg';
+import pro from '../assets/pro.jpg';
+import pro2 from '../assets/pro2.jpg';
+import opd2 from '../assets/opd2.jpg';
+import lab1 from '../assets/lab1.jpg';
+import scan1 from '../assets/scan1.jpg';
 
 const Gallery = () => {
   const [activeIndex, setActiveIndex] = useState(0);
@@ -7,15 +15,48 @@ const Gallery = () => {
   const galleries = [
     {
       id: 1,
-      img: medi,
+      title: 'OPD',
+      img: opd,
     },
     {
       id: 2,
-      img: medi,
+      title: 'LABORATORY',
+      img: lab,
     },
     {
       id: 3,
-      img: medi,
+      title: 'SCAN ROOM',
+      img: scan,
+    },
+    {
+      id: 4,
+      title: 'DISPENSARY',
+      img: despen,
+    },
+    {
+      id: 5,
+      title: 'PRODUCTION CENTER',
+      img: pro,
+    },
+    {
+      id: 6,
+      title: 'PRODUCTION CENTER',
+      img: pro2,
+    },
+    {
+      id: 7,
+      title: 'OPD',
+      img: opd2,
+    },
+    {
+      id: 8,
+      title: 'LABORATORY',
+      img: lab1,
+    },
+    {
+      id: 9,
+      title: 'SCAN ROOM',
+      img: scan1,
     },
   ];
 
@@ -44,6 +85,7 @@ const Gallery = () => {
         >
           {galleries.map((gallery) => (
             <div className="testimony image" key={gallery.id}>
+              <h3>{gallery.title}</h3>
               <img src={gallery.img} alt="Episodic" />
             </div>
           ))}
